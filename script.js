8d286fce04331e58e82c922ee8719ee66ebb1a70
NNTeamstudio <jhonierdev2.0@gmail.com>
2024-07-05 15:05:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -44,16 +44,16 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
         embeds: [{
            title: `**Reporte Nene 2.0**`,
            fields: [
-            { name: `🪪 • Nombre:`, value: `${nombre}` },
-            { name: `📧 • Correo:`, value: `${correo}` },
-            { name: `📰 • Reporte:`, value: `${reporte}` },
+            { name: `<:nombre:1258874000994471986> | Nombre:`, value: `${nombre}` },
+            { name: `<:correo:1258873856576327690> | Correo:`, value: `${correo}` },
+            { name: `<:reported:1258874106015645808> | Asunto:`, value: `${reporte}` },
            ],
            thumbnail: {
-            url: `https://cdn.discordapp.com/icons/1175511103971799100/f893fe0e5d36ba6fe2548919598067d2.webp?size=2048`,
+            url: `https://cdn.discordapp.com/attachments/1256676366993064026/1258876035164934274/78bf843475bd589a636ee5e5f65c9d35.gif?ex=6689a32b&is=668851ab&hm=86edce64a22d8f942420e4104c5b7ed37405f538ab515df0a2399491840239f1&`,
            },
            footer: {
-            text: `🔎 • Reporte ${nombre}`,
-            icon_url: `https://cdn.discordapp.com/icons/1175511103971799100/f893fe0e5d36ba6fe2548919598067d2.webp?size=2048`,
+            text: ` Reporte De ${nombre}`,
+            icon_url: `https://cdn.discordapp.com/attachments/1256676366993064026/1258876035512930455/729883918295236649.png?ex=6689a32b&is=668851ab&hm=af235a69a4f3c8585ea9916490d65d4b3282c8b72361f6816233b21edd2a71f0&`,
            },
            timestamp: new Date().toISOString()
         }]
